81438c01b651e4ba4a6504aa1c4ab8a421da725d
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-01-19 15:14:57 -0500

    completed all tasks

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { deleteMovie } from '../actions/movieActions';
+import { addFavorite } from '../actions/favoritesActions';
 
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const movies = props.movies;
+    // const movies = props.movies;
+    const { movies, displayFavorites, deleteMovie, addFavorite} = props
     const movie = movies.find(movie=>movie.id===Number(id));
     
     const handleDelete = ()=>{
-        props.deleteMovie(movie.id);
+        deleteMovie(movie.id);
         push('/movies');
+    }
+
+    const handleFavClick = ()=>{
+        addFavorite(movie);
     }
     
     return(<div className="modal-page col">
@@ -44,7 +50,7 @@ const Movie = (props) => {
                         </section>
                         
                         <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
+                            {displayFavorites && <span onClick={handleFavClick} className="m-2 btn btn-dark">Favorite</span>}
                             <span onClick={handleDelete} className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
@@ -56,8 +62,9 @@ const Movie = (props) => {
 
 const mapStateToProps = state =>{
     return({
-        movies: state.movies
+        displayFavorites: state.favReducer.displayFavorites,
+        movies: state.movieReducer.movies
     });
 }
 
-export default connect(mapStateToProps, {deleteMovie})(Movie);
+export default connect(mapStateToProps, {deleteMovie, addFavorite})(Movie);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
+import favReducer from './favoritesReducer';
+const rootReducer = combineReducers({
+    movieReducer,
+    favReducer
+})
 
-export default movieReducer;
+export default rootReducer;
